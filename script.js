1dc7f6ebb7dfc02e6360a905e641f48bb870cae7
hishanto <husain.imambd@gmail.com>
2022-06-10 13:53:17 +0600

    JS- Btn - adding +/- reset function

--- script.js
+++ script.js
@@ -5,3 +5,18 @@ let count = 0;
 const value = document.querySelector('#value');
 const btns = document.querySelectorAll('.btn');
 
+btns.forEach(function (btn) {
+    btn.addEventListener('click', function (e) {
+        style = e.currentTarget.classList;
+        if (style.contains('decrease')) {
+            count--;
+        }
+        else if (style.contains('increase')) {
+            count++;
+        }
+        if (style.contains('reset')) {
+            count = 0;
+        }
+        value.textContent = count;
+    })
+});
